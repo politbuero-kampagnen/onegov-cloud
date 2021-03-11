8e47487316ba2b61ad1d4eb658be06bf37d96fcc
Marc Sommerhalder <marc.sommerhalder@seantis.ch>
2021-03-11 07:44:35 +0100

    Fix test [skip ci]

--- src/onegov/election_day/forms/election.py
+++ src/onegov/election_day/forms/election.py
@@ -175,6 +175,8 @@ class ElectionForm(Form):
     )
 
     def parse_colors(self, text):
+        if not text:
+            return {}
         result = dict(findall(r'(.+)\s+(\#[0-9a-fA-F]{6})', text))
         if len(text.strip().splitlines()) != len(result):
             raise ValueError('Could not parse colors')
